485b1bce9ac6cca1264dda238b44da18de48f89c
NaumanMunir9 <naumanmunir9@gmail.com>
2021-09-14 11:07:35 +0500

    fetching single movie

--- pages/[movie_id].js
+++ pages/[movie_id].js
@@ -1,17 +1,7 @@
 // libraries
-import { Fragment } from "react";
 import Head from "next/head";
-import { useRouter } from "next/router";
 
-// lib
-import clientPromise from "../lib/mongodb";
-
-export default function MovieDetailsPage({ movies }) {
-  const router = useRouter();
-  const movieID = router.query;
-
-  console.log(movieID);
-
+export default function MovieDetailsPage({ movie }) {
   return (
     <div>
       <Head>
@@ -20,21 +10,12 @@ export default function MovieDetailsPage({ movies }) {
       </Head>
 
       <div className="container mx-auto">
-        <h1 className="text-5xl">Movie Details For: </h1>
+        <h1 className="text-5xl">Movie Details For: {movie.title}</h1>
       </div>
 
       <div className="container mx-auto">
         <div className="flex flex-wrap my-16">
-          {movies &&
-            movies.map((movie) => (
-              <Fragment key={movie._id}>
-                <div className="w-1/4 p-16 border border-black">
-                  <h2>Movie Title: {movie.title}</h2>
-                  <p>Release Year: {movie.year}</p>
-                  <p>IMDB Rating: {movie.imdb.rating}</p>
-                </div>
-              </Fragment>
-            ))}
+          <img src={movie.poster} alt={movie.title} />
         </div>
       </div>
     </div>
@@ -42,18 +23,13 @@ export default function MovieDetailsPage({ movies }) {
 }
 
 export async function getServerSideProps(context) {
-  const client = await clientPromise;
-  const db = client.db("sample_mflix");
-
-  const data = await db
-    .collection("movies")
-    .find({ year: { $gte: 2000 }, "imdb.rating": { $gte: 8 } })
-    .limit(20)
-    .toArray();
-
-  const movies = JSON.parse(JSON.stringify(data));
+  const data = await fetch(
+    `http://localhost:3000/api/moviedetails?movie_id=573a13bdf29313caabd582fd`
+  );
+  const movie = await data.json();
+  console.log(movie);
 
   return {
-    props: { movies },
+    props: { movie },
   };
 }
--- pages/api/moviedetails.js
+++ pages/api/moviedetails.js
@@ -1,15 +1,18 @@
+// libraries
+import { ObjectId } from "mongodb";
+
 // lib
 import clientPromise from "../../lib/mongodb";
 
 export default async function handler(req, res) {
+  const query = req.query.movie_id;
+
   const client = await clientPromise;
   const db = client.db("sample_mflix");
 
   const data = await db
     .collection("movies")
-    .find({ year: { $gte: 2000 }, "imdb.rating": { $gte: 8 } })
-    .limit(20)
-    .toArray();
+    .findOne({ _id: new ObjectId(query) });
 
   res.json(data);
 }

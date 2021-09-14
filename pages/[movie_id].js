c1b41bb00eedeec6127b7e43adca965604ebe50d
NaumanMunir9 <naumanmunir9@gmail.com>
2021-09-14 12:12:22 +0500

    getStaticPaths and getStaticProps

--- pages/[movie_id].js
+++ pages/[movie_id].js
@@ -2,6 +2,8 @@
 import Head from "next/head";
 
 export default function MovieDetailsPage({ movie }) {
+  if (!movie) return <h2>Loading...</h2>;
+
   return (
     <div>
       <Head>
@@ -22,14 +24,21 @@ export default function MovieDetailsPage({ movie }) {
   );
 }
 
-export async function getServerSideProps(context) {
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params }) {
   const data = await fetch(
-    `http://localhost:3000/api/moviedetails?movie_id=${context.query.movie_id}`
+    `http://localhost:3000/api/moviedetails?movie_id=${params.movie_id}`
   );
   const movie = await data.json();
-  console.log(movie);
 
   return {
     props: { movie },
+    revalidate: 1,
   };
 }

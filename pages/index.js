27e280e26bf4d42e34b5c909145ac80e4f426047
NaumanMunir9 <naumanmunir9@gmail.com>
2021-09-15 14:05:26 +0500

    cleaning up and making modular components

--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,24 @@
 // libraries
-import { Fragment } from "react";
 import Head from "next/head";
-import { useRouter } from "next/router";
-import Image from "next/image";
 
 // lib
 import clientPromise from "../lib/mongodb";
 
-export default function HomePage({ movies }) {
-  const router = useRouter();
+// components
+import Header from "../components/Header";
+import MoviesCard from "../components/MoviesCard";
 
+export default function HomePage({ movies }) {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>MFlix Movies</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="container mx-auto">
-        <h1 className="text-5xl">NextJS and MongoDB</h1>
-      </div>
+      <Header />
 
-      <div className="container mx-auto">
-        <div className="flex flex-wrap my-16">
-          {movies &&
-            movies.map((movie) => (
-              <Fragment key={movie._id}>
-                <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl my-8">
-                  <div className="md:flex">
-                    <div className="md:flex-shrink-0">
-                      <img
-                        className="h-full w-full object-cover md:w-48"
-                        src={movie.poster}
-                        alt={movie.title}
-                      />
-                    </div>
-                    <div className="p-8">
-                      <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-                        IMDB Rating: {movie.imdb.rating}
-                      </div>
-                      <div className="block mt-1 text-lg leading-tight font-medium text-black">
-                        {movie.title} ({movie.year})
-                      </div>
-                      <p className="mt-2 text-gray-500">{movie.plot}</p>
-                      <button
-                        onClick={() => router.push(`/${movie._id}`)}
-                        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mt-4 w-full"
-                      >
-                        View
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </Fragment>
-            ))}
-        </div>
-      </div>
+      <MoviesCard movies={movies} />
     </div>
   );
 }
@@ -66,7 +29,7 @@ export async function getServerSideProps(context) {
 
   const data = await db
     .collection("movies")
-    .find({ year: { $gte: 2000 }, "imdb.rating": { $gte: 8 } })
+    .find({ year: { $gte: 2000 }, "imdb.rating": { $gte: 8.2 } })
     .limit(20)
     .toArray();
 

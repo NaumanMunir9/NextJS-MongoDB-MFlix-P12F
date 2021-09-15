aa3d702b2dd40795591fcb58ca6e5e9d17de8c27
NaumanMunir9 <naumanmunir9@gmail.com>
2021-09-15 13:11:30 +0500

    Header

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,53 @@
-// styles
-import "../styles/Header.module.css";
-
 export default function Header() {
   return (
-    <div className="navigation">
-      <div className="container">
-        <div className="navbar">
-          <div className="nav-logo">MNM</div>
+    <div className="relative bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+          <div className="flex justify-start lg:w-0 lg:flex-1">
+            <a href="#">
+              <span className="sr-only">Workflow</span>
+              <img
+                className="h-8 w-auto sm:h-10"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                alt="logo"
+              />
+            </a>
+          </div>
 
-          <div className="nav-list">
-            <a href="#" className="nav-list-item">
+          <div as="nav" className="hidden md:flex space-x-10">
+            <a
+              href="#"
+              className="text-base font-medium text-gray-500 hover:text-gray-900"
+            >
               Home
+            </a>
+
+            <a
+              href="#"
+              className="text-base font-medium text-gray-500 hover:text-gray-900"
+            >
+              About Us
+            </a>
+            <a
+              href="#"
+              className="text-base font-medium text-gray-500 hover:text-gray-900"
+            >
+              404 Page
+            </a>
+          </div>
+
+          <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+            <a
+              href="#"
+              className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+            >
+              Sign in
+            </a>
+            <a
+              href="#"
+              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+            >
+              Sign up
             </a>
           </div>
         </div>
